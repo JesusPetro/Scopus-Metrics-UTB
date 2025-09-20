5518bafec1e01a4d72ade95516d4994394f5e0d3
JesusPetro <petrojesus094@gmail.com>
2025-09-20 11:27:53 -0500

    remove print

--- test/__init__.py
+++ test/__init__.py
@@ -6,7 +6,6 @@ from log_util import LOGGERS, add_file_handle_to_logger
 def log_to_directory(dirpath, logger_names = None):
     if logger_names is None:
         logger_names = LOGGERS.keys()
-    print(LOGGERS)
     
     assert all(ln in LOGGERS for ln in logger_names), \
         f"Unknow loggers! {logger_names}"
